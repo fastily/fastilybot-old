898f48b2e759979056958b1d881cba6ae3210bef
fastily <fastily@users.noreply.github.com>
2017-06-20 17:11:15 -0700

    initial commit

---
+++ src/main/java/enwp/bots/FlagOI.java
@@ -0,0 +1,41 @@
+package enwp.bots;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import fastily.ctools.util.Toolbox;
+import fastily.enwp.WTP;
+import fastily.jwiki.core.MQuery;
+import fastily.jwiki.core.NS;
+import fastily.jwiki.core.Wiki;
+
+/**
+ * Finds and flags orphaned free media files on enwp.
+ * 
+ * @author Fastily
+ *
+ */
+public class FlagOI
+{
+	/**
+	 * The Wiki object to use
+	 */
+	private static Wiki wiki = Toolbox.getFastilyBot();
+
+	/**
+	 * Main driver
+	 * 
+	 * @param args Program arguments, not used.
+	 */
+	public static void main(String[] args)
+	{
+		HashSet<String> l = Toolbox.fetchLabsReportListAsFiles(wiki, "report3");
+		l.removeAll(WTP.orphan.getTransclusionSet(wiki, NS.FILE));
+		l.removeAll(WTP.nobots.getTransclusionSet(wiki, NS.FILE));
+		l.removeAll(Toolbox.fetchLabsReportListAsFiles(wiki, "report4"));
+		l.removeAll(new HashSet<>(MQuery.exists(wiki, false, new ArrayList<>(l))));
+		
+		for(String s : l)
+			wiki.addText(s, "\n{{Orphan image}}", "BOT: Noting that file has no inbound file usage", false);
+	}
+}
---
+++ src/main/java/enwp/bots/UnflagOI.java
@@ -0,0 +1,55 @@
+package enwp.bots;
+
+import java.util.HashSet;
+
+import fastily.ctools.util.Toolbox;
+import fastily.enwp.WTP;
+import fastily.jwiki.core.MQuery;
+import fastily.jwiki.core.NS;
+import fastily.jwiki.core.Wiki;
+
+/**
+ * Removes {{Orphan image}} from freely licensed files which contain file links in the main space.
+ * 
+ * @author Fastily
+ *
+ */
+public final class UnflagOI
+{
+	/**
+	 * The Wiki object to use
+	 */
+	private static final Wiki wiki = Toolbox.getFastilyBot();
+
+	/**
+	 * A regex matching the Orphan image template
+	 */
+	private static final String oiRegex = WTP.orphan.getRegex(wiki);
+
+	/**
+	 * Main driver
+	 * 
+	 * @param args Program arguments, unused.
+	 */
+	public static void main(String[] args)
+	{
+		HashSet<String> oL = Toolbox.fetchLabsReportListAsFiles(wiki, "report3");
+		oL.removeAll(Toolbox.fetchLabsReportListAsFiles(wiki, "report4"));
+		
+		HashSet<String> l = WTP.orphan.getTransclusionSet(wiki, NS.FILE);
+		l.removeAll(oL);
+		l.removeAll(WTP.nobots.getTransclusionSet(wiki, NS.FILE));
+
+		
+		System.out.println(l.size());
+		
+		for(String s : l)
+			System.out.println(s);
+		
+		
+//		MQuery.fileUsage(wiki, wiki.whatTranscludesHere(WTP.orphan.title, NS.FILE)).forEach((k, v) -> {
+//			if(!wiki.filterByNS(v, NS.MAIN).isEmpty())
+//				wiki.replaceText(k, oiRegex, "BOT: File contains inbound links");
+//		});
+	}
+}
